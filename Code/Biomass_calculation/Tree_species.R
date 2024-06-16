0c5d07d503e179a8d4a7516ba332af0212192661
Sarvnazsadat  Roumianfar <161208063+SarvnazRmn@users.noreply.github.com>
2024-06-16 10:28:36 +0200

    Add files via upload

--- Code/Biomass_calculation/Tree_species.R
+++ Code/Biomass_calculation/Tree_species.R
@@ -1,252 +1,250 @@
-library(Matrix)
-library(lme4)
-library(lmerTest)
+# this section has to be run only the first time
+# install.packages("remotes")
+# remotes::install_github("ropensci/allodb")
+#-------------------------------------------------------------------------------
+# libraries used
 library(effects)
 library(plotrix)
 library(gridExtra)
-library(piecewiseSEM)
-library(nlme)
-library(pgirmess)
-library(splines)
-library(car)
-library(ISLR)
-library(lattice)
-library(latticeExtra)
-library(HH)
-library(multcomp)
-library(openxlsx)
-library(multcompView)
-library(dplyr)
-library(ggplot2)
-library(ARTool)
-library(sciplot)
-library(MASS)
-library(nortest)
-library(performance)
-library(ggpubr)
-library(devtools)
-library(factoextra)
-library(vegan)
-library(permute)
-library(lattice)
-library(corrplot)
-library(emmeans)
-library(lmerTest)
 library(kgc)
 library(dplyr)
 library(readxl)
+library(allodb)
+library(ggplot2)
+library(maps)
+library(scales)
+library(knitr)
+library(kableExtra)
+library(osmdata)
+library(leaflet)
+library(leaflet.extras)
+library(maps)
 
-#---------------------------------------------------------------------------------------------------------------------------------------
-# DATI
+#-------------------------------------------------------------------------------
+# CHOOSE YOUR SITE LOCATION
 
-# questi dati provengono dalla repository di Github del pacchetto allobd
+#insert here the name of the nearest city
+data(world.cities)
 
-load("sitespecies.rda")
-load("equations.rda")
-tab_koppen <- read_excel("tab_koppen.xlsx")
-#View(tab_koppen)
+# find the coordinates
+city_read <- readline(prompt="Enter the nearest city name: ")
+city <- world.cities[world.cities$name == city_read, ]
 
 
-# questi sono i miei dati prelevati dalle matrici 
+# map with site
+leaflet() %>%
+  addTiles() %>%
+  setView(lng = city$long, lat = city$lat, zoom = 11) %>%
+  addMarkers(lng = city$long, lat = city$lat) %>%
+  addMiniMap(width = 150, height = 150)
 
-speci <- (equations$equation_taxa)
-equazioni <- (equations$equation_allometry)
-clima <- (equations$koppen)
-unità <-(equations$output_units_original)
-dbhunità <- (equations$dbh_units_original)
+#-------------------------------------------------------------------------------
+# UPLOADING OF THE TREE SPECIES IDENTIFICATION RESULTS
 
-# li rendo una matrice
-dat <- tibble(speci,clima,equazioni,unità,dbhunità)
+# insert here your file with the tree species
+message("Choose an excel file with tree species")
+file_path <- file.choose()
 
-#---------------------------------------------------------------------------------------------------------------------------------------
-# SCELTA DELLA REGIONE
+# read the Excel file
+tryCatch({
+  tab_species_1 <- readxl::read_excel(file_path)
+  print("File read correctly!")
+  print(head(tab_species_1))  # print the first lines of the database
+}, error = function(e) {
+  print(paste("Error during the reading of the Excel file:", e))
+})
 
-# matrice di koppen -> scelta 
+#dividing the latin names in genus and species
+genus <- vector("character", length = nrow(tab_species_1)) 
+species <- vector("character", length = nrow(tab_species_1))  
 
-opzioni <- c("Abruzzo","Basilicata","Calabria","Campania","Emilia-Romagna","Friuli-Venezia Giulia","Lazio","Liguria","Lombardia",
-             "Marche","Molise","Piemonte","Puglia","Sardegna","Sicilia","Toscana","Trentino-Alto Adige","Umbria","Valle d’Aosta",
-             "Veneto")
-
-# faccio scegliere all'utente la regione
-regione <- menu(opzioni, title = "Seleziona un'opzione: ")
-print(paste("Hai scelto:", opzioni[regione]))
-
-#---------------------------------------------------------------------------------------------------------------------------------------
-# SCREMATURA DEL DATASET
-
-# ricerco le posizioni utilizzando le climate zones 
-
-# koppen climate zones 
-# Af, Am, Aw/As, BWh, BWk, BSh, BSk, Csa, Csb, Csc, Cwa, Cwb, Cwc, Cfa,
-# Cfb, Cfc, Dsa, Dsb, Dsc, Dsd, Dwa, Dwb, Dwc, Dwd, Dfa, Dfb, Dfc, Dfd, ET, EF
-
-scelta_clima = tab_koppen$`climate zone`[regione]
-print(scelta_clima)
-
-# divido il vettore
-scelta_clima <- unlist(strsplit(scelta_clima, ";"))
-
-# prealloco
-posizioni <- c()
-
-# per ogni valore del vettore scelta_clima cerco le posizioni
-for(x in scelta_clima){
-  pos <- grep(x,dat$clima)
-  posizioni <- c(posizioni, pos)
+for (i in 1:nrow(tab_species_1)) {
+  species_value <- tab_species_1$species[i]
+  parts <- unlist(strsplit(species_value, " "))
+  genus[i] <- parts[1]  
+  species[i] <- parts[2]  
 }
 
-# metto a posto il vettore posizioni, in ordine crescente e senza le ripetizioni
-posizioni <- sort(posizioni)
-posizioni <- unique(posizioni)
+tab_species <- cbind(genus,species)
+tab_species <- data.frame(tab_species)
 
+#---------------------------------------------------------------------------------------------------------------------------------------
+# DATA
 
-# inserisco il vettore char di speci presenti nella foresta
+# area covered by the point cloud
+# given as result from Cloudcompare with 3DFin
+area <- 1695 #[m^2]
 
-speci_insert <- c("Sapindacee","","");
+# table of conversion for units
+conv_dbh <- read.csv2("conv_dbh.csv")
 
+# choose the diameters file from Cloudcompare with 3DFin
+message("Choose a csv file with the tree dbh")
+file_path <- file.choose()
 
+# read the csv file
+tryCatch({
+  tab_dbh <- read.csv2(file_path)
+  print("File read correclty!")
+  print(head(tab_dbh))  # print the first lines of the database
+}, error = function(e) {
+  print(paste("Error during the reading of the Excel file:", e))
+})
 
+# rearrangement of the dataset
+result <- tab_dbh
+result$X <- as.numeric(result$X)
+result$Y <- as.numeric(result$Y)
+result$GEN <- tab_species$genus[1]
+result$SPEC <- tab_species$species[1]
+
+dbh_values <- as.numeric(result$DBH) 
+
+# choose the unit of dbh
+a <- readline(prompt="Enter the unit of dbh (m, cm, mm, inch): ")
+a <- which(conv_dbh$X == a)
+
+# based on the unit that we have, we should convert it to cm because it is requested by get_biomass function
+b <- which(colnames(conv_dbh) == 'cm')
+dbh_values <- dbh_values*as.numeric(conv_dbh[a,b]) # from meters to centimeters
 
 
 #---------------------------------------------------------------------------------------------------------------------------------------
-# esempio con ippocastano -> aesculus hippocastanus, famiglia Sapindaceae -> ho delle equazioni 
+# BIOMASS ESTIMATION
 
-hip_pos <- grep("Sapindaceae",dat$speci)
+agb <- get_biomass(dbh_values, 
+                       result$GEN, 
+                       coords = c(city$lat,city$long), 
+                       result$SPEC, 
+                       wna = 0.1, w95 = 500, nres = 10000)
+result$AGB <- agb
 
-# prealloco
-eq_hip <- c()
+# total biomass of the forest
+total_biomass <- sum(agb)
 
-for(x in hip_pos) {
-  print(x)
-  eq <- dat$equazioni[x]
-  eq_hip <- c(eq_hip,eq)
-}
+# biomass per area [m^2]
+biomass_perm2 <- total_biomass/area
 
-eq_hip <- sort(eq_hip)
-eq_hip <- unique(eq_hip)
+#-------------------------------------------------------------------------------
+# MAP OF THE AREA
 
-n = length(eq_hip)
+# coordinates obtained manually through google maps
+polygon_coords <- list(
+  cbind(c(7.664122, 7.664257, 7.665523, 7.665444), c(45.063845, 45.063991, 45.063514, 45.063389))
+)
+coord <- c(45.063845, 7.664122)
 
+# popup for the biomass marker on the map 
+biomass_tot <- round(sum(agb))
+biomass_tot <- as.character(biomass_tot)
+biomass_tot <- c("The total biomass is",biomass_tot, "[Kg]")
+biomass_tot <- paste(biomass_tot, collapse = " ")
 
-# diametri presi random ippocastani
+# popup for the number of trees on the map
+num_trees <- length(agb)
+num_trees <- c("The number of trees is:",num_trees)
+num_trees <- paste(num_trees, collapse = " ")
 
-dbh <- c(25, 30, 22, 28, 33, 20, 26, 31, 24, 29) #[m]
-
-
-# lo rendo un vettore numerico
-
-eq_hip_fin <- c() # prealloco
-
-for(x in dbh) {
-  eq_hip <- sapply(eq_hip, function(x) eval(parse(text = x)))
-  #print(eq_hip)
-  eq_hip_fin <- c(eq_hip_fin,eq_hip)
-}
-
-eq_hip =eq_hip_fin
-eq_hip <- unique(eq_hip)
-
-e = split(eq_hip,rep(1:n,each = length(dbh)))
-
-vec1 = e$`1`
-vec2 = e$`2`
+# popup content
+popup_content <- paste0(
+  "<div style='color: blue; background-color: yellow; padding: 5px; border-radius: 5px;'>",
+  biomass_tot,
+  "</div>"
+)
 
 
-
-#dbh = as.character(dbh)
+# visualizing the biomass
+leaflet() %>%
+  addTiles() %>%
+  setView(lng = coord[2], lat = coord[1], zoom = 25) %>%
+  addMarkers(lng = coord[2], lat = coord[1], popup = popup_content) %>%
+  addPolygons(
+    lng = polygon_coords[[1]][, 1],
+    lat = polygon_coords[[1]][, 2],
+    color = "blue",
+    weight = 2,
+    fillColor = "lightblue",
+    fillOpacity = 0.5,
+    highlightOptions = highlightOptions(
+      color = "red",
+      weight = 3,
+      bringToFront = TRUE
+    ),
+    popup = num_trees
+  ) %>%
+  addMiniMap(width = 150, height = 150)
 
 
 #---------------------------------------------------------------------------------------------------------------------------------------
 # BAR PLOTTING
 
+ggplot(data = result, aes(x = factor(DBH), y = AGB, fill = SPEC)) +
+  geom_bar(stat = "identity", color = "black") +
+  scale_fill_discrete() + 
+  labs(title = "Biomass calculation",
+       x = "Tree diameter [cm]",
+       y = "Biomass [Kg]") +
+  theme(axis.text.x = element_text(angle = 90, hjust = 1, size = 8))
 
-barplot(vec1, names.arg = dbh, col = "#556B2F",
-        main = "Biomass calculation for different hippocastanus trees",
-        xlab = "Tree diameter", ylab = "Biomass [Kg]")
 
 
-barplot(vec2, names.arg = dbh, col = "#9ACD32",
-        main = "Biomass calculation for different hippocastanus trees",
-        xlab = "Tree diameter [m]", ylab = "Biomass [Kg]")
+#---------------------------------------------------------------------------------------------------------------------------------------
+# STACK BAR PLOTTING
+category <- c('trees')
 
+ggplot(result, aes(fill=T, y=AGB, x=category)) + 
+  geom_bar(position="stack", stat="identity", color="black") +
+  theme(panel.background = element_rect(fill = "white"),
+        panel.grid.major = element_line(colour = "grey"),
+        panel.grid.minor = element_line(colour = "grey"),
+        plot.title = element_text(face = "bold", hjust = 0.5)) +
+  geom_text(aes(label=T), position=position_stack(vjust=0.5), size=3) +
+  theme(legend.position="none") + 
+  labs(x="Category", y="AGB [kg]", fill="Trees", title="Total Biomass of the forest")
 
 #---------------------------------------------------------------------------------------------------------------------------------------
 # BOX PLOTTING
 
-boxplot(cbind(vec1,vec2), main = "Biomass calculation for different hippocastanus trees",
-        xlab = "Tree", ylab = "Biomass [Kg]",
-        col = c("#9ACD32", "#556B2F"), border = "black", notch = FALSE, xaxt = "n")
-
-
-legend("topright", legend = c("eq_1", "eq_2"), fill = c("#9ACD32", "#556B2F"))
-
+boxplot(result$AGB, 
+        main = "Average biomass estimation",
+        xlab = "Trees", ylab = "Biomass [kg]",
+        col = c("#83A655"), border = "black", notch = FALSE, xaxt = "n")
 
 #---------------------------------------------------------------------------------------------------------------------------------------
-# Larix Cfa -> 52 + Acer saccharum -> 181
+# SCATTER PLOT
 
+p <- ggplot(result, aes(x=X, y=Y)) +
+  geom_point(aes(size = AGB), colour = c('#83A655')) +
+  #scale_colour_gradientn(colours = c('#83A655')) +
+  #theme(plot.background = element_rect(fill = "#F6E9D3")) +
+  theme(panel.background = element_rect(fill = "white"),
+        panel.grid.major = element_line(colour = "grey"),
+        panel.grid.minor = element_line(colour = "grey"),
+        plot.title = element_text(face = "bold", hjust = 0.5)) +
+  #theme(text = element_text(family = "Montserrat"))
+  geom_text(aes(label = T), vjust = 1.5, color = "black") +
+  scale_size_continuous(name = "AGB [kg]") +
+  scale_x_continuous(labels = number_format(accuracy = 1)) +
+  scale_y_continuous(labels = number_format(accuracy = 1)) +
+  ggtitle("Spatial distribution of biomass") +
+  xlab("X coordinate") +
+  ylab("Y coordinate")
+  
+print(p)
 
-lar_pos <- which(dat$speci == "Larix")
+#---------------------------------------------------------------------------------------------------------------------------------------
+# TABLE
 
-lar_pos <- lar_pos[1]
-print(lar_pos)
+table_res <- data.frame( number_trees = length(dbh_values), 
+                        area, 
+                        total_biomass, 
+                        biomass_perm2)
 
+colnames(table_res) <- rep("&nbsp;", ncol(table_res))
 
-ac_pos <- which(dat$speci == "Pinus palustris")
-
-print(ac_pos)
-
-eq_lar = dat$equazioni[lar_pos]
-eq_ac = dat$equazioni[lar_pos]
-
-u_lar = dat$unità[lar_pos]
-u_ac = dat$unità[ac_pos]
-
-uu_lar = dat$dbhunità[lar_pos]
-uu_ac = dat$dbhunità[ac_pos]
-
-dbh <- c(24, 73, 42, 11, 86, 57, 35, 68, 19, 91) #[cm]
-
-e_lar <- c()
-
-for(x in 1) {
-  eq_lar <- sapply(eq_lar, function(x) eval(parse(text = x)))
-  print(eq_lar)
-  e_lar <- c(eq_lar)
-}
-
-barplot(e_lar, names.arg = dbh, col = "#556B2F",
-        main = "Biomass calculation for Larix",
-        xlab = "Tree diameter [cm]", ylab = "Biomass [kg]")
-
-
-
-
-dbh <- c(24, 50, 42, 11, 45, 19, 35, 30, 23, 49) 
-
-
-e_ac <- c()
-
-for(x in 1) {
-  eq_ac <- sapply(eq_ac, function(x) eval(parse(text = x)))
-  print(eq_ac)
-  e_ac <- c(eq_ac)
-}
-
-
-
-
-barplot(e_ac, names.arg = dbh, col = "#9ACD32",
-        main = "Biomass calculation for Pinus palustris",
-        xlab = "Tree diameter [cm]", ylab = "Biomass [Kg]")
-
-
-
-boxplot(cbind(e_lar,e_ac), main = "Biomass calculation",
-        xlab = "Tree", ylab = "Biomass",
-        col = c("#556B2F","#9ACD32"), border = "black", notch = FALSE, xaxt = "n")
-
-
-legend("topright", legend = c("Larix", "Pinus palustris"), fill = c("#556B2F","#9ACD32"))
-
-
-
+# Generate the table
+kable(table_res, "html", escape = F, caption = "Results") %>%
+  kable_styling("striped", bootstrap_options = c("striped", "hover"), 
+                position = "left") %>%
+  add_header_above(c("Number of trees" = 1,"Area [m^2]" = 1,"Total Biomass [kg]" = 1,"Biomass Density [kg/m^2]" = 1)) %>% 
+  row_spec(0, extra_css = "border-color: #83A655;")
